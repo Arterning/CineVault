838dbd278abc3b3d90425f5af527542bd8c70a3f
Arterning <ning.huang.ridson@gmail.com>
2026-02-09 21:14:02 +0800

    feat: 增加文件上传进度显示和内存限制设置
    - 在Http类中添加onWorkerStart方法设置内存和执行时间限制
    - 在电影创建页面添加文件上传进度条显示功能
    - 使用AJAX处理大文件上传并显示实时进度

--- app/process/Http.php
+++ app/process/Http.php
@@ -6,5 +6,21 @@ use Webman\App;
 
 class Http extends App
 {
-
+    /**
+     * OnWorkerStart.
+     * @param $worker
+     * @return void
+     */
+    public function onWorkerStart($worker)
+    {
+        // 设置内存限制
+        ini_set('memory_limit', '1512M');
+        ini_set('upload_max_filesize', '1500M');
+        ini_set('post_max_size', '1500M');
+        ini_set('max_execution_time', '300');
+        ini_set('max_input_time', '300');
+        
+        // 调用父类方法
+        parent::onWorkerStart($worker);
+    }
 }
